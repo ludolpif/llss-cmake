703b7060a7263d96739d21ec5e156a48d22ecd1a
ludolpif <ludolpif@gmail.com>
2026-08-06 10:34:14 +0200

    Get rid of CMakePresets.json that have some combinatorial verbosity

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
-Configuration="${1:-RelWithDebInfo}"
-set -xe
-cd -- "$ROOT_DIR"
-VERBOSE=1 cmake --preset=Linux -DCMAKE_C_FLAGS="-march=x86-64-v3" -DCMAKE_CXX_FLAGS="-march=x86-64-v3"
-VERBOSE=1 cmake --build --preset=Linux --config $Configuration
-VERBOSE=1 cmake --install build --config $Configuration --prefix "$ROOT_DIR/install/$Configuration"
+GITROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+cfg="${1:-RelWithDebInfo}"
+
+# Stop the script when a native command fails
+set -xe -o pipefail
+
+export CMAKE_GENERATOR="Ninja Multi-Config"
+export VERBOSE=1
+
+cd -- "$GITROOT"
+cmake -B ./build \
+	-DCMAKE_C_COMPILER="gcc" \
+	-DCMAKE_CXX_COMPILER="g++" \
+	-DCMAKE_C_FLAGS="-march=x86-64-v3" \
+	-DCMAKE_CXX_FLAGS="-march=x86-64-v3" \
+	-S .
+
+cmake --build ./build --config $cfg
+
+pushd ./build
+ctest --build-config $cfg
+popd
+
+cmake --install ./build --config $cfg --prefix "$GITROOT/install/$cfg"
--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -1,28 +1,48 @@
 param (
-    [Parameter(Mandatory = $false)]
-    [string]$Configuration
+	[Parameter(Mandatory = $false)]
+	[string]$cfg
 )
 
-if (!$PSBoundParameters.ContainsKey('Configuration')) {
-    $Configuration = "RelWithDebInfo"
+$GITROOT = "$PSScriptRoot/.."
+if (!$PSBoundParameters.ContainsKey('cfg')) {
+	$cfg = "RelWithDebInfo"
 }
-if ($Configuration -ne "Debug" -and $Configuration -ne "RelWithDebInfo" -and $Configuration -ne "Release") {
-    [Console]::Error.WriteLine("Usage: $PSCommandPath (Debug|RelWithDebInfo|Release)")
-    exit 1
+if ($cfg -ne "Debug" -and $cfg -ne "RelWithDebInfo" -and $cfg -ne "Release") {
+	[Console]::Error.WriteLine("Usage: $PSCommandPath (Debug|RelWithDebInfo|Release)")
+	exit 1
 }
 
-"::notice Set-Location $PSScriptRoot\.."
-pushd "$PSScriptRoot\.."
+# Stop the script when a native command or a cmdlet fails
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+"::notice $env:CMAKE_GENERATOR = Visual Studio 18 2026"
+$env:CMAKE_GENERATOR = "Visual Studio 18 2026"
+"::notice $env:VERBOSE = 1"
+$env:VERBOSE = 1
+
+"::notice cd $GITROOT"
+cd $GITROOT
 
 # powershell "&" is call operator, not unix background task meta-character
 
-'::notice --preset=Windows -DCMAKE_C_FLAGS="/arch:AVX2" -DCMAKE_CXX_FLAGS="/arch:AVX2"'
-& cmake --preset=Windows -DCMAKE_C_FLAGS="/arch:AVX2" -DCMAKE_CXX_FLAGS="/arch:AVX2"
+'::notice cmake -B ./build -D[...] -S .'
+& cmake -B ./build `
+	-DCMAKE_C_COMPILER="cl" `
+	-DCMAKE_CXX_COMPILER="cl" `
+	-DCMAKE_C_FLAGS="/arch:AVX2" `
+	-DCMAKE_CXX_FLAGS="/arch:AVX2" `
+	-S .
 
-"::notice  --build --preset=Windows --config $Configuration"
-& cmake --build --preset=Windows --config $Configuration
+"::notice cmake --build ./build --config $cfg"
+& cmake --build ./build --config $cfg
 
-"::notice cmake --install build --config $Configuration --prefix install/$Configuration"
-& cmake --install build --config $Configuration --prefix install/$Configuration
-
+"::notice pushd ./build"
+pushd ./build
+"::notice ctest --build-config $cfg"
+& ctest --build-config $cfg
+"::notice popd"
 popd
+
+"::notice cmake --install build --config $cfg --prefix $GITROOT/install/$cfg"
+& cmake --install ./build --config $cfg --prefix "$GITROOT/install/$cfg"
